4d965f74584729159885250dc1a1675109c4ae6f
Marc Cortés <marc.cortes@enti.cat>
2025-03-24 13:25:27 +0100

    Muerte Enemigo

---
+++ Assets/Script/BalaEnemy.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DestruirAlColisionar : MonoBehaviour
+{
+    // Método que se llama cuando ocurre una colisión con otro objeto
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Comprobar si el objeto con el que colisionamos tiene el tag "Bala"
+        if (collision.gameObject.CompareTag("BalaPlayer"))
+        {
+            // Destruir el objeto con el que este script está asociado (el objeto que lleva el script)
+            Destroy(gameObject);
+        }
+    }
+}
